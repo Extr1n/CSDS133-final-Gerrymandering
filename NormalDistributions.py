bd67916fa2fdc5cd7a2c0129d22975946c358741
Aidan Bugayong <aidan.bugayong@gmail.com>
2024-11-22 18:18:55 -0500

    more plots :)

--- NormalDistributions.py
+++ NormalDistributions.py
@@ -4,23 +4,41 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 
-# data = pd.read_csv('data.csv')
-# mean = data['Column1'].mean()
-# std = data['Column1'].std()
-
+#data placeholder
+count = 100
+data = pd.DataFrame({"Red": np.random.normal(20, 20, count), "Blue": np.random.normal(170, 20, count)})
 #scale x-axis for plot
-x_axis = np.arange(00, 200, 0.1)
+x_axis = np.arange(-60, 260, 0.1)
+bins = 20
 
-blue_mean = 50      # placeholder
-blue_std = 20        # placeholder
-actual_blue= 20    # placeholder
+blue_mean = data["Blue"].mean()
+blue_std = data["Blue"].std()
+actual_blue= 170    # placeholder
 
-red_mean = 150      # placeholder
-red_std = 20        # placeholder
-actual_red= 170    # placeholder
+red_mean = data["Red"].mean()
+red_std = data["Red"].std()
+actual_red= 20    # placeholder
 
+#normal distributions
 plt.plot(x_axis, norm.pdf(x_axis, loc=blue_mean, scale=blue_std), color='b')
 plt.axvline(x=actual_blue, color='b', linestyle='dashed', linewidth=1)
 plt.plot(x_axis, norm.pdf(x_axis, loc=red_mean, scale=red_std), color='r')
+plt.axvline(x=actual_red, color='r', linestyle='dashed', linewidth=1)
+plt.figure()
+
+
+#histogram plot
+plt.axvline(x=actual_blue, color='b', linestyle='dashed', linewidth=1)
+plt.axvline(x=actual_red, color='r', linestyle='dashed', linewidth=1)
+plt.hist(data["Blue"], bins=bins, color='b', alpha=0.3, edgecolor='black')
+plt.hist(data["Red"], bins=bins, color='r', alpha=0.3, edgecolor='black')
+plt.figure()
+
+#combined plot
+plt.hist(data["Blue"], bins=bins, color='b', density=True, alpha=0.3, edgecolor='black')
+plt.hist(data["Red"], bins=bins, color='r', density=True, alpha=0.3, edgecolor='black')
+plt.plot(x_axis, norm.pdf(x_axis, loc=blue_mean, scale=blue_std), color='b', linewidth=1)
+plt.axvline(x=actual_blue, color='b', linestyle='dashed', linewidth=1)
+plt.plot(x_axis, norm.pdf(x_axis, loc=red_mean, scale=red_std), color='r', linewidth=1)
 plt.axvline(x=actual_red, color='r', linestyle='dashed', linewidth=1)
 plt.show()
