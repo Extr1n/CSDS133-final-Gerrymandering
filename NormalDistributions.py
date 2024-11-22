ace241241425607b5137ae39b6bb6afc70ac1f2b
Aidan Bugayong <aidan.bugayong@gmail.com>
2024-11-22 15:01:07 -0500

    Added Red + Blue to same Normal graph

--- NormalDistributions.py
+++ NormalDistributions.py
@@ -11,11 +11,16 @@ from scipy.stats import norm
 #scale x-axis for plot
 x_axis = np.arange(00, 200, 0.1)
 
-mean = 100      # placeholder
-std = 20        # placeholder
+blue_mean = 50      # placeholder
+blue_std = 20        # placeholder
+actual_blue= 20    # placeholder
 
-actual_count= 20    # placeholder
+red_mean = 150      # placeholder
+red_std = 20        # placeholder
+actual_red= 170    # placeholder
 
-plt.plot(x_axis, norm.pdf(x_axis, loc=mean, scale=std), color='b')
-plt.axvline(x=actual_count, color='k', linestyle='dashed', linewidth=1)
+plt.plot(x_axis, norm.pdf(x_axis, loc=blue_mean, scale=blue_std), color='b')
+plt.axvline(x=actual_blue, color='b', linestyle='dashed', linewidth=1)
+plt.plot(x_axis, norm.pdf(x_axis, loc=red_mean, scale=red_std), color='r')
+plt.axvline(x=actual_red, color='r', linestyle='dashed', linewidth=1)
 plt.show()
